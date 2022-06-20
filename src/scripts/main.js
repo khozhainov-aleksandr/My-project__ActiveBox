ff18dab598a540b14e964e4df29b688ae7bd203e
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-06-20 21:04:18 +0300

    Moving the project and first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menuToggle = document.querySelector('#menu-togle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function() {
+  menuToggle.classList.toggle('menu-icon-active');
+  mobileNavContainer.classList.toggle('mobile-nav--active');
+};
